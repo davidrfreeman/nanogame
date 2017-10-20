a009bac2fa9763640bd6ced1b41d2a790fb0b393
davidrfreeman <davidfreeman@holos.solutions>
2017-10-19 22:36:51 -0600

    Modified server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+    return (req.headers['x-forwarded-proto'] === 'https') ? res.redirect('http://' + req.hostname + req.url) : next();
+  });
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
